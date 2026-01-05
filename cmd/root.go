b917eae788eadb554818c02ee724dffc7812c0ca
Matt Krueger <mkrueger@rstms.net>
2026-01-05 14:56:35 -0700

    Bump version: 0.0.1 → 0.0.2

--- cmd/root.go
+++ cmd/root.go
@@ -40,7 +40,7 @@ import (
 var cfgFile string
 
 var rootCmd = &cobra.Command{
-	Version: "0.0.1",
+	Version: "0.0.2",
 	Use:     "smtpd-filter-spamclass",
 	Short:   "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
